c0fb66021ee12910ff89cc31ae719999c2e92070
Chaitanya Teke <127778433+Chaitanyateke@users.noreply.github.com>
2025-08-21 17:32:13 +0530

    Delete C Module/Assignments directory

--- C Module/Assignments/Assignment 0/q6.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-void main(){
-	int n=10,sq,cube;
-	sq=n*n;
-	cube=n*n*n;
-	printf("%d square=%d and cube=%d", n,sq,cube);
-}
--- C Module/Assignments/Assignment 0/q9.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-void main(){
-	double b=15,h=18,area;
-	area = 0.5*b*h;
-	printf("Area of triangle with %lf base and %lf height = %lf", b,h,area);
-}
