d7016515fbde6bd4082db0466f43a2176a1840be
Chaitanya Teke <127778433+Chaitanyateke@users.noreply.github.com>
2025-08-21 09:32:37 +0530

    Add files via upload

---
+++ C Module/Assignments/Assignment 1/q3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main(){
+	int year=2006;
+	
+	if(year%4 == 0 && year % 100 != 0 || year % 400 == 0){
+		printf("%d is a leap year",year);
+	}
+	else{
+		printf("%d is not a leap year",year);
+	}
+}
---
+++ C Module/Assignments/Assignment 1/q5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main(){
+	int age=17;
+	
+	if(age>18){
+		printf("Person is eligible to vote");
+	}
+	else{
+		printf("Person is not eligible to vote");
+	}
+}
